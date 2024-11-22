ca594080591a295b6bdb79caa7966fe5cfe7d716
adogbeji <baka_1993@yahoo.co.uk>
2024-11-22 15:19:30 +0000

    Create new variable and function in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        Fundamental React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
@@ -14,15 +19,6 @@ function Header() {
 function App() {
   return (
     <div>
-      {/* <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header> */}
-
       <Header/>
       <main>
         <h2>Time to get started!</h2>
