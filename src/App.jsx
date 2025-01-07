3a0a37d6d7453e488941f9d484a92afbe1e145fe
adogbeji <baka_1993@yahoo.co.uk>
2025-01-07 13:51:09 +0000

    Output dynamic data in CoreConcept component

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,9 @@ function Header() {
 
 function CoreConcept(props) {
   return <li>
-    <img src={props.image} alt="..." />
-    <h3>TITLE</h3>
-    <p>DESCRIPTION</p>
+    <img src={props.image} alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
   </li>;
 }
 
