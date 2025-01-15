f315c844b5d8d10ee84e30dd4cfea5ee70b3ea28
adogbeji <baka_1993@yahoo.co.uk>
2025-01-15 13:47:11 +0000

    Use shorthand syntax to pass props to other CoreConcept components

--- src/App.jsx
+++ src/App.jsx
@@ -37,27 +37,15 @@ function App() {
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
-          <CoreConcept 
+          {/* <CoreConcept 
             title={CORE_CONCEPTS[0].title} 
             description={CORE_CONCEPTS[0].description} 
             img={CORE_CONCEPTS[0].image}
-          />
-          {/* <CoreConcept 
-            title={CORE_CONCEPTS[1].title} 
-            description={CORE_CONCEPTS[1].description} 
-            img={CORE_CONCEPTS[1].image}
           /> */}
           <CoreConcept {...CORE_CONCEPTS[0]}/>
-          <CoreConcept 
-            title={CORE_CONCEPTS[2].title} 
-            description={CORE_CONCEPTS[2].description} 
-            img={CORE_CONCEPTS[2].image}
-          />
-          <CoreConcept 
-            title={CORE_CONCEPTS[3].title} 
-            description={CORE_CONCEPTS[3].description} 
-            img={CORE_CONCEPTS[3].image}
-          />
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+          <CoreConcept {...CORE_CONCEPTS[3]}/>
         </section>
         {/* <h2>Time to get started!</h2> */}
       </main>
