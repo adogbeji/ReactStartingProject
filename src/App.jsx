b290ac99b7c2bb3bc1894b4c7f5c95ecdd47f508
adogbeji <baka_1993@yahoo.co.uk>
2025-02-07 13:47:24 +0000

    Put CoreConcept component in separate file

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,8 @@
 // import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
-
-function CoreConcept({ title, description, image }) {
-  return <li>
-    <img src={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>;
-}
 
 function App() {
   return (
